e1f245a73bd19fece8f2aa73ce1e37a7f4a0bfa7
mobilars <lars@roland.bz>
2020-04-12 00:11:04 +0200

    Shows map of Oslo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-import * as L from 'leaflet';
+import { Component, OnInit } from "@angular/core";
+import * as L from "leaflet";
+import "leaflet.markercluster";
+import { PopupComponent } from "./components/popup/popup.component";
+import { NgElement, WithProperties } from "@angular/elements";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  map: L.Map;
+
+  ngOnInit() {
+    //this.init();
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map("map", {
+      center: [59.96051, 10.639229],
+      zoom: 12
+    });
+    const tiles = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }
+    );
+
+    tiles.addTo(this.map);
+  }
 }
